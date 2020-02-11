4da412c2594d8a5e8ce1bebe32563927a5e0e45b
faustin0 <dinatalefausto@gmail.com>
2020-02-11 21:21:35 +0100

    adding email dto & model

---
+++ src/main/java/it/faustino/emailsender/dtos/EmailDTO.java
@@ -0,0 +1,47 @@
+package it.faustino.emailsender.dtos;
+
+public class EmailDTO {
+    private String from;
+    private String to;
+    private String body;
+    private String subject;
+
+
+    public EmailDTO(String from, String to, String body) {
+        this.from = from;
+        this.to = to;
+        this.body = body;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}
