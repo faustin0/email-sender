db3384396d2a26c9052762a5f7942778e8432726
faustin0 <dinatalefausto@gmail.com>
2020-02-12 22:43:39 +0100

    fixing startup, again

--- src/test/java/it/faustino/emailsender/EmailSenderApplicationTests.java
+++ src/test/java/it/faustino/emailsender/EmailSenderApplicationTests.java
@@ -1,12 +1,11 @@
 package it.faustino.emailsender;
 
-import it.faustino.emailsender.services.impl.EmailSenderImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {EmailSenderImpl.class})
+@SpringBootTest
 class EmailSenderApplicationTests {
 
     @Autowired
